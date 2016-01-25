4812c880e57badb4a797e9f70cc551235868b43a
Jose Uusitalo <joseu.git@gmail.com>
2016-01-25 16:14:51 +0200

    Initial main controller

---
+++ src/velho/controller/Velho.java
@@ -0,0 +1,20 @@
+package velho.controller;
+/**
+ * The main controller for the VELHO Warehouse Management software.
+ *
+ * @author Jose Uusitalo
+ */
+public class Velho
+{
+	/**
+	 * The main method for running this software.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Running VELHO Warehouse Management.");
+		// Do stuff.
+		System.out.println("Done!");
+	}
+}
