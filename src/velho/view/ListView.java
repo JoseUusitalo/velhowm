1f4cc409817195f768a99080b20e689ea1344531
Jose Uusitalo <joseu.git@gmail.com>
2016-01-29 20:02:25 +0200

    Syntax and warning fixes

--- src/velho/view/ListView.java
+++ src/velho/view/ListView.java
@@ -6,41 +6,28 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import velho.controller.ListViewController;
-import velho.controller.UIController;
-import velho.controller.UserController;
 import velho.model.Manager;
 import velho.model.User;
 
 public class ListView
 {
-	private ListViewController controller;
-
+	private ListViewController listController;
 	private BorderPane pane;
 	private ObservableList<User> datalist;
 	private List<String> columnNames;
 
-	public ListView(ListViewController listController, ObservableList list)
+	public ListView(ListViewController listController, ObservableList<User> list)
 	{
 		columnNames = new ArrayList<String>(Arrays.asList("First name", "Last name"));
 		this.datalist = list;
-		this.datalist = FXCollections.observableArrayList(new User(12, "Joona", "Silv", new Manager("Juu")), new User(50, "Joona2", "Silv2", new Manager("Juu2")));
-		controller = listController;
+		// TODO: Dev code.
+		this.datalist = FXCollections.observableArrayList(new User(12, "Joona", "Silv", new Manager()), new User(50, "Joona2", "Silv2", new Manager()));
+		this.listController = listController;
 		pane = null;
 	}
 
@@ -50,21 +37,20 @@ public class ListView
 		{
 			pane = new BorderPane();
 
-			List<TableColumn> cols = new ArrayList<TableColumn>();
-			
-			for(int i=0; i<columnNames.size(); i++)
+			List<TableColumn<User, String>> cols = new ArrayList<TableColumn<User, String>>();
+
+			for (int i = 0; i < columnNames.size(); i++)
 			{
-				TableColumn<User, String> col = new TableColumn<User, String>(columnNames.get(1));
-				col.setCellValueFactory(
-		                new PropertyValueFactory<User, String>(columnNames.get(1)));
+				TableColumn<User, String> col = new TableColumn<User, String>(columnNames.get(i));
+				col.setCellValueFactory(new PropertyValueFactory<User, String>(columnNames.get(i)));
 				cols.add(col);
 			}
-			
-			TableView tableView = new TableView();
+
+			TableView<User> tableView = new TableView<User>();
 			tableView.setItems(datalist);
-			tableView.getColumns().addAll(cols);		
+			tableView.getColumns().addAll(cols);
 			pane.setCenter(tableView);
-			
+
 		}
 		return pane;
 	}
