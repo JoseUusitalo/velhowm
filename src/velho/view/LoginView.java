79ffd615adc0c557891ecbb3702a8855790cd614
Jose Uusitalo <joseu.git@gmail.com>
2016-02-01 13:07:19 +0200

    Changed login textfield to passwordfield

--- src/velho/view/LoginView.java
+++ src/velho/view/LoginView.java
@@ -5,7 +5,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.GridPane;
 import velho.controller.LoginController;
 
@@ -36,7 +36,7 @@ public class LoginView
 
 			grid.add(labels, 0, 1, 1, 1);
 
-			final TextField textField = new TextField();
+			final PasswordField textField = new PasswordField();
 			grid.add(textField, 0, 2, 2, 1);
 
 			Button logInButton = new Button("Log In");
