b7dd5d498079ff8866752fc342a9b9fb780e7d55
Jose Uusitalo <joseu.git@gmail.com>
2016-02-04 11:53:37 +0200

    Removed a JavaFX controller test from the suite

--- src/test/controller/AllControllerTests.java
+++ src/test/controller/AllControllerTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DatabaseControllerTest.class, UserControllerTest.class })
+@SuiteClasses({ DatabaseControllerTest.class })
 public class AllControllerTests
 {
 	// Class for running all controller tests in the project.
