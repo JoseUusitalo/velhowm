fc04db3fc672e89afb382842d9acf66ee1f4a72a
Jose Uusitalo <joseu.git@gmail.com>
2016-02-04 11:29:40 +0200

    One working JavaFX unit test

--- src/test/controller/UserControllerTest.java
+++ src/test/controller/UserControllerTest.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
+import javafx.application.Application;
 import velho.controller.DatabaseController;
+import velho.controller.PopupController;
 import velho.controller.UserController;
+import velho.model.JavaFXThreadingRule;
 import velho.model.exceptions.ExistingDatabaseLinkException;
 import velho.model.exceptions.NoDatabaseLinkException;
 
@@ -16,22 +20,23 @@ import velho.model.exceptions.NoDatabaseLinkException;
  *
  * @author Jose Uusitalo
  */
-@SuppressWarnings("static-method")
 public class UserControllerTest
 {
+	@Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+
 	private static UserController controller;
 
-	private final String VALID_BADGE_ID = "12345678";
-	private final String INVALID_BADGE_ID_SHORT = "12345";
-	private final String INVALID_BADGE_ID_LONG = "123456789";
+	private final String VALID_BADGE_ID = "99955999";
+	private final String INVALID_BADGE_ID_SHORT = "15";
+	private final String INVALID_BADGE_ID_LONG = "1234567891";
 
-	private final String VALID_PIN = "123456";
+	private final String VALID_PIN_MAX = "999999";
+	private final String VALID_PIN_MIN = "000000";
 	private final String INVALID_PIN_SHORT = "12";
 	private final String INVALID_PIN_LONG = "1234567";
 
 	private final String VALID_NAME = "First-Name";
 	private final String INVALID_NAME_LONG = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-	private final String INVALID_NAME_NULL = null;
 
 	private final String VALID_ROLE_NAME = "Manager";
 	private final String INVALID_ROLE_NAME = "Worker";
@@ -52,12 +57,6 @@ public class UserControllerTest
 	@Test
 	public final void testAddUserValid()
 	{
-		fail("Do not know how to test GUI controllers yet.");
-		/*
-		 * DatabaseController.link();
-		 * DatabaseController.initializeDatabase();
-		 * assertTrue(controller.addUser(VALID_BADGE_ID, null, VALID_NAME, VALID_NAME, VALID_ROLE_NAME));
-		 * DatabaseController.unlink();
-		 */
+		assertTrue(controller.addUser(VALID_BADGE_ID, null, VALID_NAME, VALID_NAME, VALID_ROLE_NAME));
 	}
 }
