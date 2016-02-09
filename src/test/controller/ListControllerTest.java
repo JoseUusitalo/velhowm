6caf832c5113c6cf53aae2eca966bb79007423f1
Jose Uusitalo <joseu.git@gmail.com>
2016-02-09 15:16:44 +0200

    Initial product list search view and logic

---
+++ src/test/controller/ListControllerTest.java
@@ -0,0 +1,78 @@
+package test.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import velho.controller.ListController;
+
+/**
+ * Tests for the {@link ListController} class.
+ *
+ * @author Jose Uusitalo
+ */
+public class ListControllerTest
+{
+	private static final String PRODUCT_1 = "Product 1";
+	private static final String PRODUCT_2 = "Product 2";
+	private static final String PRODUCT_3 = "Product 3";
+	private static final String PRODUCT_4 = "Product 4";
+	private static final String PRODUCT_TWO_COLONS = "A Very Long Product Name: With Two: Colons";
+	private static final String PRODUCT_WITH_COLON = "Product Name: With a Single Colon";
+
+	private ListController listController = new ListController(null);
+
+	@Test
+	public final void testSearchByProductList()
+	{
+		final Map<String, Integer> expected = new LinkedHashMap<String, Integer>();
+		expected.put(PRODUCT_1, 1);
+		expected.put(PRODUCT_2, 2);
+		expected.put(PRODUCT_3, 3);
+		expected.put(PRODUCT_4, 1);
+		// @formatter:off
+		final String string = "1: " + PRODUCT_1 + "\n"
+							+ "2: " + PRODUCT_2 + "           \n"
+							+ "3: " + PRODUCT_3 + "\n"
+							+ PRODUCT_4 + "\n";
+		// @formatter:on
+
+		assertEquals(expected, listController.searchByProductList(string));
+	}
+
+	@Test
+	public final void testSearchByProductList_Duplicates()
+	{
+		final Map<String, Integer> expected = new LinkedHashMap<String, Integer>();
+		expected.put(PRODUCT_1, 8);
+		expected.put(PRODUCT_2, 2);
+		// @formatter:off
+		final String string = PRODUCT_1 + "\n"
+							+ "2: " + PRODUCT_2 + "           \n"
+							+ "3: " + PRODUCT_1 + "\n"
+							+ "4: " + PRODUCT_1 + "\n";
+		// @formatter:on
+
+		assertEquals(expected, listController.searchByProductList(string));
+	}
+
+	@Test
+	public final void testSearchByProductList_NameWithColon()
+	{
+		final Map<String, Integer> expected = new LinkedHashMap<String, Integer>();
+		expected.put("Product: A Colon!", 1);
+		expected.put(PRODUCT_WITH_COLON, 10);
+		expected.put(PRODUCT_TWO_COLONS, 1);
+
+		// @formatter:off
+		final String string = "Product: A Colon!" + "\n"
+							+ "10: " + PRODUCT_WITH_COLON + "\n"
+							+ PRODUCT_TWO_COLONS + "\n";
+		// @formatter:on
+
+		assertEquals(expected, listController.searchByProductList(string));
+	}
+}
--- src/velho/controller/ListController.java
+++ src/velho/controller/ListController.java
@@ -1,11 +1,13 @@
 package velho.controller;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import velho.model.User;
 import velho.view.ListView;
+import velho.view.ProductListSearch;
 
 /**
  * A class for controlling the creation of lists.
@@ -58,5 +60,101 @@ public class ListController
 	public void removeUser(final User user)
 	{
 		userController.removeUser(user);
+	}
+
+	/**
+	 * Gets the view for searching for multiple products at once.
+	 *
+	 * @return product list search view
+	 */
+	public Node getProductListSearchView()
+	{
+		ProductListSearch list = new ProductListSearch(this);
+		return list.getProductListSearch();
+	}
+
+	/**
+	 * Searches the database for the given products.
+	 *
+	 * @param products a string of product names or IDs (one per line)
+	 * @return
+	 */
+	public Map<String, Integer> searchByProductList(final String products)
+	{
+		String[] productStringLines = products.split("\n");
+		Map<String, Integer> productData = new LinkedHashMap<String, Integer>();
+
+		// Convert lines to a map.
+		for (final String line : productStringLines)
+		{
+			// Count first, then product.
+			String[] possibleProductAndCount = line.split(":");
+			int count = 1;
+			String productString = null;
+
+			if (possibleProductAndCount.length == 1)
+			{
+				// Example: 'Product A'
+				productString = possibleProductAndCount[0].trim();
+			}
+			else if (possibleProductAndCount.length == 2)
+			{
+				// Example: '15: Product B'
+				// Example: 'Product C: Long Name'
+				try
+				{
+					count = Integer.valueOf(possibleProductAndCount[0]);
+					productString = possibleProductAndCount[1].trim();
+				}
+				catch (NumberFormatException e)
+				{
+					// Count remains 1.
+					productString = (possibleProductAndCount[0] + ":" + possibleProductAndCount[1]).trim();
+				}
+			}
+			else
+			{
+				// Example: '90: Product C: The Better Version'
+				// Example: 'Product D: Cool 'n Stuff: Too Many Colons Version'
+				int start = 0;
+				try
+				{
+					count = Integer.valueOf(possibleProductAndCount[0]);
+					start = 1;
+				}
+				catch (NumberFormatException e)
+				{
+					// Count remains 1.
+				}
+
+				// Rebuild the product string.
+				StringBuffer sb = new StringBuffer();
+				int length = possibleProductAndCount.length;
+
+				for (int i = start; i < length; i++)
+				{
+					if (start == 1 && i == 1)
+					{
+						// Left trim spaces because the first element was a number.
+						sb.append(possibleProductAndCount[i].replaceAll("^\\s+", ""));
+					}
+					else
+						sb.append(possibleProductAndCount[i]);
+
+					if (i < length - 1)
+						sb.append(":");
+				}
+				productString = sb.toString();
+			}
+
+			// If the product already exists, add the new count to the previous count.
+			if (productData.containsKey(productString))
+				productData.put(productString, productData.get(productString) + count);
+			else
+				productData.put(productString, count);
+		}
+
+		// Return the data for unit testing.
+		return productData;
 	}
 }
