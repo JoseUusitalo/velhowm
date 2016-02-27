87dc6dd63c60cbb06ce4ccdd1e103e0280de0274
Jose Uusitalo <joseu.git@gmail.com>
2016-02-27 14:29:16 +0200

    Log4j properties, database table, gitignore

---
+++ data/loginit.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS `syslogs`
+(
+	`time` TIMESTAMP NOT NULL,
+	`logger` VARCHAR(1024) NOT NULL,
+	`level` VARCHAR(64) NOT NULL,
+	`message` TEXT NOT NULL,
+
+	PRIMARY KEY (`time`,`logger`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `usrlogs`
+(
+	`user_id` INT UNSIGNED NOT NULL,
+	`time` TIMESTAMP NOT NULL,
+	`logger` VARCHAR(1024) NOT NULL,
+	`level` VARCHAR(64) NOT NULL,
+	`message` TEXT NOT NULL,
+
+	PRIMARY KEY (`user_id`,`time`,`logger`)
+) DEFAULT CHARSET=utf8;
