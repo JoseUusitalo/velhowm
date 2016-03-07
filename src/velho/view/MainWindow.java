2fe71d9f6eecd0217873798ec6e806813b82f487
Jose Uusitalo <joseu.git@gmail.com>
2016-03-07 14:51:38 +0200

    Disabled debug mode

--- src/velho/view/MainWindow.java
+++ src/velho/view/MainWindow.java
@@ -65,7 +65,7 @@ public class MainWindow extends Application
 	/**
 	 * Enable or disable debug features.
 	 */
-	public static final boolean DEBUG_MODE = true;
+	public static final boolean DEBUG_MODE = false;
 
 	/**
 	 * Enable or disable showing windows. DEBUG_MODE must be <code>true</code>
@@ -77,7 +77,7 @@ public class MainWindow extends Application
 	 * Enable TRACE level logging. DEBUG_MODE must be <code>true</code> for this
 	 * to affect anything.
 	 */
-	public static final boolean SHOW_TRACE = true;
+	public static final boolean SHOW_TRACE = false;
 
 	/**
 	 * The height of the window.
